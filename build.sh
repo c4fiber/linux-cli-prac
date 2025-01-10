c1dfdf9db2863326dceae798e28b175b02274a29
c4fiber <qudcjf153@gmail.com>
2025-01-10 17:48:14 +0900

    Create image build script
    빌드 간소화를 위해서 배쉬 스크립트 작성.

---
+++ build.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+# Only 1 arguments: version number
+if [ $# -ne 1 ]; then
+	echo "usage: ./build.sh <version>"
+	exit 1
+fi
+
+# Is it demical number?
+if ! [[ $1 =~ ^[0-9]+(\.[0-9]+)?$ ]]; then
+	echo "Error: Argument must be a demical number"
+	exit 1
+fi
+
+# Is Dockerfile exist?
+if [ ! -f $DOCKERFILE ]; then
+        echo "Error: Dockerfile is not exist"
+        exit 1
+fi
+
+# Variables
+REPOSITORY=hayanbada/linux-cli-prac
+TAG=$1
+DOCKERFILE=./Dockerfile
+
+# Check if version is already exist
+DOES_EXIST=$(docker image ls -a | grep hayanbada/linux-cli-prac | awk '{print $2}' | grep $TAG | wc -l)
+if [ $DOES_EXIST -gt 0 ]; then
+	echo "Error: version already exist"
+	exit 1
+fi
+
+# NOTE
+# For if condition test
+#echo "start build (fake)"
+#exit 0
+
+# Build image and replace latest image
+echo "====== start build ======"
+
+if [ $(docker image ls $REPOSITORY:latest | wc -l) -ne 0 ]; then
+	docker image rm $REPOSITORY:latest
+fi
+
+docker buildx build --progress=plain --platform linux/amd64,linux/arm64 -t $REPOSITORY:$TAG -t $REPOSITORY:latest --push .
