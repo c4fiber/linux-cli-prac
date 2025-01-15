d980a8b9198b27dd24cca8789dbeb2f311c23732
c4fiber <qudcjf153@gmail.com>
2025-01-15 14:04:59 +0900

    Refactor build.sh and run_container.sh
    apply linux kernel coding style and add some comments

--- build.sh
+++ build.sh
@@ -1,28 +1,30 @@
 #!/bin/bash
+#
+# Created by c4fiber<qudcjf153@gmail.com>
+# Shell script for build docker image and push to docker hub easily
+#
+# Usage: ./build.sh <tag_or_version>
 
-# more than 1 arguments
 if [ $# -lt 1 ]; then
 	echo "usage: ./build.sh <version> [user_id:user_password]"
 	exit 1
 fi
 
-# Is it demical number?
 if ! [[ $1 =~ ^[0-9]+(\.[0-9]+)?$ ]]; then
 	echo "Error: Argument must be a demical number"
 	exit 1
 fi
 
-# Is Dockerfile exist?
 if [ ! -f ./Dockerfile ]; then
 	echo "Error: Dockerfile is not exist"
 	exit 1
 fi
 
-# Variables
+# variables for build docker image
 IMAGE_NAME=hayanbada/linux-cli-prac
 TAG=$1
 
-# Check if version is already exist
+# check if same docker image:tag already exists
 docker image ls -a | grep $IMAGE_NAME | awk '{print $2}' | grep $TAG > /dev/null
 if [ $? -eq 0 ]; then
 	echo "Error: version already exist"
@@ -36,7 +38,7 @@ fi
 # Build image and replace latest image
 echo "====== start build ======"
 docker buildx build --progress=plain \
-  --platform linux/amd64,linux/arm64 \
-  -t $IMAGE_NAME:$TAG \
-  -t $IMAGE_NAME:latest \
-  --push .
+	--platform linux/amd64,linux/arm64 \
+	-t $IMAGE_NAME:$TAG \
+	-t $IMAGE_NAME:latest \
+	--push .
--- run_container.sh
+++ run_container.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+#
+# shell script for run container or start&attach container easily
+#
+# Usage: ./run_container.sh
 # create by c4fiber<qudcjf153@gmail.com>
 
 CONTAINER_NAME=lets-practice-linux-cli
@@ -8,18 +12,21 @@ VOLUME=linux-cli-prac_data
 # create volume if it is not exist
 docker volume inspect linux-cli-prac_data > /dev/null
 if [ $? -eq 0 ]; then
-  echo "volume: linux-cli-prac_data already exist"
+	echo "volume: linux-cli-prac_data already exist"
 else
-  echo "create new volume: $VOLUME"
-  docker volume create "$VOLUME" > /dev/null
+	echo "create new volume: $VOLUME"
+	docker volume create "$VOLUME" > /dev/null
 fi
 
 # Does container already exist?
-EXISTING_CONTAINER=$(docker ps -a --filter "name=^${CONTAINER_NAME}$" --format "{{.Names}}")
+EXISTING_CONTAINER=$(docker ps -a \
+			--filter "name=^${CONTAINER_NAME}$" \
+			--format "{{.Names}}")
 if [ "$EXISTING_CONTAINER" = "$CONTAINER_NAME" ]; then
-  echo "container: $CONTAINER_NAME already exists "
-  docker start -ai "$CONTAINER_NAME"
+	echo "container: $CONTAINER_NAME already exists "
+	docker start -ai "$CONTAINER_NAME"
 else
-  echo "create new container: $CONTAINER_NAME"
-  docker run --name "$CONTAINER_NAME" -it -v $VOLUME:/home/myuser "$IMAGE_NAME" /bin/bash
+	echo "create new container: $CONTAINER_NAME"
+	docker run -it --name "$CONTAINER_NAME" \
+		-v $VOLUME:/home/myuser "$IMAGE_NAME" /bin/bash
 fi
